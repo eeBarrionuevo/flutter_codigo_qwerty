2c0f4669ac38ca3bc7754e17a3222df33ad49892
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-08-17 19:44:58 -0500

    Animation 2

--- lib/pages/animation_controller.dart
+++ lib/pages/animation_controller.dart
@@ -1,6 +1,4 @@
-
 import 'package:flutter/material.dart';
-
 
 class AnimationControllerPage extends StatefulWidget {
   const AnimationControllerPage({Key? key}) : super(key: key);
@@ -10,14 +8,50 @@ class AnimationControllerPage extends StatefulWidget {
       _AnimationControllerPageState();
 }
 
-class _AnimationControllerPageState extends State<AnimationControllerPage> {
+class _AnimationControllerPageState extends State<AnimationControllerPage> with SingleTickerProviderStateMixin {
+
+  //AnimationController
+  //Animation
+
+  late AnimationController pruebaAnimationController;
+  late Animation pruebaAnimation ;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    pruebaAnimationController = AnimationController(vsync: this, duration: const Duration(milliseconds: 5000));
+    pruebaAnimation = ColorTween(begin: Colors.pinkAccent, end: Colors.indigo,).animate(pruebaAnimationController);
 
 
+
+    pruebaAnimationController.addListener(() {
+      //print(pruebaAnimationController.value);
+      // print(pruebaAnimation.value);
+    });
+
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
+        child: AnimatedBuilder(
+          animation: pruebaAnimation,
+          builder: (BuildContext context, _){
+            print(pruebaAnimation.value);
+            return IconButton(
+              onPressed: () {
+                pruebaAnimationController.forward();
+              },
+              icon: Icon(
+                Icons.star,
+                size: 34.0,
+                color: pruebaAnimation.value,
+              ),
+            );
+          },
+        ),
       ),
     );
   }
