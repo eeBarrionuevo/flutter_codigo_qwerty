1ff45ce94a199155771daf5284bc036187e8fe6c
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-08-17 20:01:02 -0500

    Animation 3

--- lib/pages/animation_controller.dart
+++ lib/pages/animation_controller.dart
@@ -13,22 +13,34 @@ class _AnimationControllerPageState extends State<AnimationControllerPage> with 
   //AnimationController
   //Animation
 
-  late AnimationController pruebaAnimationController;
-  late Animation pruebaAnimation ;
+  // late AnimationController pruebaAnimationController;
+  // late Animation pruebaAnimation ;
+
+  late AnimationController _turnController;
+  late Animation<double> _turnAnimation;
+
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    pruebaAnimationController = AnimationController(vsync: this, duration: const Duration(milliseconds: 5000));
-    pruebaAnimation = ColorTween(begin: Colors.pinkAccent, end: Colors.indigo,).animate(pruebaAnimationController);
+    // pruebaAnimationController = AnimationController(vsync: this, duration: const Duration(milliseconds: 5000));
+    // pruebaAnimation = ColorTween(begin: Colors.pinkAccent, end: Colors.indigo,).animate(pruebaAnimationController);
+
+    _turnController = AnimationController(vsync: this, duration: const Duration(milliseconds: 4000))..repeat(reverse: true);
+    _turnAnimation = CurvedAnimation(parent: _turnController, curve: Curves.bounceOut);
 
 
+    // pruebaAnimationController.addListener(() {
+    //   //print(pruebaAnimationController.value);
+    //   // print(pruebaAnimation.value);
+    // });
 
-    pruebaAnimationController.addListener(() {
-      //print(pruebaAnimationController.value);
-      // print(pruebaAnimation.value);
-    });
+    _turnController.forward();
+    //
+    // _turnController.addListener(() {
+    //   print(_turnAnimation.value);
+    // });
 
   }
 
@@ -36,21 +48,27 @@ class _AnimationControllerPageState extends State<AnimationControllerPage> with 
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: AnimatedBuilder(
-          animation: pruebaAnimation,
-          builder: (BuildContext context, _){
-            print(pruebaAnimation.value);
-            return IconButton(
-              onPressed: () {
-                pruebaAnimationController.forward();
-              },
-              icon: Icon(
-                Icons.star,
-                size: 34.0,
-                color: pruebaAnimation.value,
-              ),
-            );
-          },
+        // child: AnimatedBuilder(
+        //   animation: pruebaAnimation,
+        //   builder: (BuildContext context, _){
+        //     print(pruebaAnimation.value);
+        //     return IconButton(
+        //       onPressed: () {
+        //         pruebaAnimationController.forward();
+        //       },
+        //       icon: Icon(
+        //         Icons.star,
+        //         size: 34.0,
+        //         color: pruebaAnimation.value,
+        //       ),
+        //     );
+        //   },
+        // ),
+        child: RotationTransition(
+          turns: _turnAnimation,
+          child: FlutterLogo(
+            size: 200,
+          ),
         ),
       ),
     );
