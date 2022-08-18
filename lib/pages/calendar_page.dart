fab0990fa61e2a2105d2cf0e06e8b9d84de5ac90
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-08-17 21:49:53 -0500

    Calendar 6

--- lib/pages/calendar_page.dart
+++ lib/pages/calendar_page.dart
@@ -13,6 +13,13 @@ class _CalendarPageState extends State<CalendarPage> {
 
   DateTime _focusedDay = DateTime.now();
   CalendarFormat _calendarFormat = CalendarFormat.month;
+  DateTime? _selectedDay;
+
+
+  List<Event> _getEventsForDay(DateTime day) {
+    // Implementation example
+    return kEvents[day] ?? [];
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,23 +33,36 @@ class _CalendarPageState extends State<CalendarPage> {
             height: 100,
           ),
           TableCalendar(
-            focusedDay: _focusedDay,
+            // locale: 'es',
             firstDay: kFirstDay,
             lastDay: kLastDay,
+            focusedDay: _focusedDay,
+            selectedDayPredicate: (day) => isSameDay(_selectedDay, day),
             headerVisible: true,
             daysOfWeekVisible: true,
             calendarFormat: _calendarFormat,
+            eventLoader: _getEventsForDay,
             headerStyle: HeaderStyle(
               formatButtonShowsNext: false,
               formatButtonVisible: false,
               titleCentered: true,
               titleTextStyle: TextStyle(
-                fontSize: 20.0
+                fontSize: 18.0
               ),
               decoration: BoxDecoration(
                 color: Colors.white,
               ),
             ),
+            onDaySelected: (selectedDay, focusedDay){
+              if (!isSameDay(_selectedDay, selectedDay)) {
+                setState(() {
+                  _selectedDay = selectedDay;
+                  _focusedDay = focusedDay;
+                  print(selectedDay);
+                  print(focusedDay);
+                });
+              }
+            },
           ),
         ],
       ),
