7ba4c4e6c4f77502a5ae3a956dc6e7b971156c41
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-08-17 21:21:45 -0500

    Excel 5

--- lib/pages/calendar_page.dart
+++ lib/pages/calendar_page.dart
@@ -1,5 +1,6 @@
-
 import 'package:flutter/material.dart';
+import 'package:qwerty/utils/utils_calendar.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class CalendarPage extends StatefulWidget {
   const CalendarPage({Key? key}) : super(key: key);
@@ -9,13 +10,42 @@ class CalendarPage extends StatefulWidget {
 }
 
 class _CalendarPageState extends State<CalendarPage> {
+
+  DateTime _focusedDay = DateTime.now();
+  CalendarFormat _calendarFormat = CalendarFormat.month;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Calendar Page"),
       ),
-
+      body: Column(
+        children: [
+          const SizedBox(
+            height: 100,
+          ),
+          TableCalendar(
+            focusedDay: _focusedDay,
+            firstDay: kFirstDay,
+            lastDay: kLastDay,
+            headerVisible: true,
+            daysOfWeekVisible: true,
+            calendarFormat: _calendarFormat,
+            headerStyle: HeaderStyle(
+              formatButtonShowsNext: false,
+              formatButtonVisible: false,
+              titleCentered: true,
+              titleTextStyle: TextStyle(
+                fontSize: 20.0
+              ),
+              decoration: BoxDecoration(
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
